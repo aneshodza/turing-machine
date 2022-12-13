99abff6a24f7c742da926bb915798afb76fc8b9a
Anes Hodza <anes.hodza@renuo.ch>
2022-12-13 23:21:48 +0100

    Memory works

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## What does a PC have?
+### Adressable memory
+The PC needs a place where it can store and read variables. The memory needs to have an address (in HEX).
+
+### Executable coding language
+The PC needs a language to write code in. The language needs to be executable by the PC. I can write my own language for it
+
+### Input/Output
+The PC needs a way to get input from the user and a way to output data to the user. The PC needs to be able to read from the keyboard and write to the screen.
+
+### Memory for the code
+The PC needs a place to store the code. The code needs to be stored in a way that the PC can read it.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::{io, collections::HashMap, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to this small virtual CPU simulator!");
+
+    let mut memory: HashMap<String, u8> = HashMap::new();
+    for x in 0..16 {
+        memory.insert(get_hex_string(x), 0);
+    }
+
+    loop {
+        let memory_ref = &memory;
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input = input.trim().to_string();
+        
+        if input.eq(":q") {
+            exit(0);
+        } else if input.eq(":mem") {
+            list_storage(memory_ref);
+        }
+
+    }
+}
+
+fn get_hex_string(num: u8) -> String {
+    if num > 16 {
+        return String::from(format!("0x{:X}", num));
+    } else {
+        return String::from(format!("0x0{:X}", num));
+    }
+}
+
+fn list_storage(memory_ref: &HashMap<String, u8>) {
+    for x in 0..16 {
+        let address = get_hex_string(x);
+        let val = memory_ref.get(&address).unwrap_or(&0);
+        println!("{}: {}", address, get_hex_string(*val))
+    }
 }

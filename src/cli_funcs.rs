e5196bd5783473b9479298744125ea80f1040f9f
Anes Hodza <anes.hodza@renuo.ch>
2022-12-14 17:33:05 +0100

    Add code storage

--- src/cli_funcs.rs
+++ src/cli_funcs.rs
@@ -1,11 +1,27 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, process::exit};
 
 use crate::utils;
 
-pub fn list_storage(memory_ref: &HashMap<String, u8>) {
+pub fn list_storage(memory_ref: &HashMap<String, u8>, params: Vec<&str>) {
     for x in 0..16 {
         let address = utils::get_hex_string(x);
         let val = memory_ref.get(&address).unwrap_or(&0);
         println!("{}: {}", address, utils::get_hex_string(*val))
     }
+}
+
+pub fn list_code(code_ref: &HashMap<String, &str>, params: Vec<&str>) {
+    for x in 0..16 {
+        let address = utils::get_hex_string(x);
+        let val = code_ref.get(&address).unwrap_or(&"---");
+        println!("{}: {}", address, val);
+    }
+}
+
+pub fn exit_code(params: Vec<&str>) {
+    exit(0);
+}
+
+pub fn help() {
+    println!("For help, read the GitHub docs")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, collections::HashMap, process::exit};
+use std::{io, collections::HashMap, process::exit, borrow::Borrow};
 
 mod cli_funcs;
 mod utils;
@@ -7,12 +7,16 @@ fn main() {
     println!("Welcome to this small virtual CPU simulator!");
 
     let mut memory: HashMap<String, u8> = HashMap::new();
+    let mut code: HashMap<String, &str> = HashMap::new();
     for x in 0..16 {
-        memory.insert(utils::get_hex_string(x), 0);
+        let addy: String = utils::get_hex_string(x);
+        memory.insert(addy.clone(), 0);
+        code.insert(addy.clone(), "---");
     }
 
     loop {
         let memory_ref = &memory;
+        let code_ref = &code;
         let mut input = String::new();
 
         io::stdin()
@@ -25,10 +29,14 @@ fn main() {
         let params: Vec<&str> = input.collect();
         
         if cmd.eq(":q") {
-            exit(0);
+            cli_funcs::exit_code(params);
         } else if cmd.eq(":mem") {
-            cli_funcs::list_storage(memory_ref);
+            cli_funcs::list_storage(memory_ref, params);
+        } else if cmd.eq(":src") {
+            cli_funcs::list_code(code_ref, params)
         }
-
+        else {
+            cli_funcs::help();
+        }
     }
 }
